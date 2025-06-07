89921b4f5a4899867739030aa2bb1534dac3f809
Max Sun <102423252+kkazoo@users.noreply.github.com>
2025-06-07 10:54:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
+<script>
+  const form = document.getElementById("opportunity-form");
+  const opportunityList = document.getElementById("opportunities-list");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // stop page refresh
+
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const location = document.getElementById("location").value;
+    const date = document.getElementById("date").value;
+    const category = document.getElementById("category").value;
+
+    // Create the opportunity card
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <h3>${title}</h3>
+      <p> ${location}</p>
+      <p> ${date}</p>
+      <p> Category: ${category}</p>
+      <p>${description}</p>
+    `;
+
+    opportunityList.appendChild(card);
+    form.reset();
+  });
+</script>
 
